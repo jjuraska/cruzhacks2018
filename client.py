a393209125d525caa5faf7b57037d3ae694cc539
Jurik Juraska <juraska.juraj@gmail.com>
2018-01-21 02:08:23 -0800

    Fixed receiving of responses from the server.

--- client.py
+++ client.py
@@ -6,7 +6,7 @@ import platform
 import requests
 import websockets
 import asyncio
-from urllib.parse import urlencode
+from threading import Thread
 
 
 class SpeechClient:
@@ -22,7 +22,7 @@ class SpeechClient:
         self.endpoint_conversation = r'wss://speech.platform.bing.com/speech/recognition/conversation/cognitiveservices/v1'
         self.endpoint_dictation = r'wss://speech.platform.bing.com/speech/recognition/dictation/cognitiveservices/v1'
 
-        self.chunk_size = 4096
+        self.chunk_size = 8192
         self.is_ongoing_turn = False
         self.cur_hypothesis = ''
         self.phrase = ''
@@ -30,16 +30,15 @@ class SpeechClient:
 
     # ---- PUBLIC METHODS ----
 
-    async def send_stt_request(self, audio_file_path):
+    async def send_stt_request(self, language, response_format, audio_file_path):
 
-        url = self.endpoint_interactive + '?language=en-US&format=simple'
+        url = self.endpoint_interactive + '?language={0}&format={1}'.format(language, response_format)
         headers = {'Authorization': 'Bearer ' + self.auth_token,
                    'X-ConnectionId': self.connection_id}
 
         async with websockets.client.connect(url, extra_headers=headers) as ws:
             try:
                 ws.handshake(url, origin='https://speech.platform.bing.com')
-
                 # DEBUG PRINT
                 # print('Handshake successful!')
                 # print(ws.host)
@@ -63,79 +62,114 @@ class SpeechClient:
             }
             payload = {'context': context}
 
+            # assemble the header for the speech-config message
             msg = 'Path: speech.config\r\n'
             msg += 'Content-Type: application/json; charset=utf-8\r\n'
             msg += 'X-Timestamp: ' + str(datetime.datetime.now())[:-3] + 'Z\r\n'
+            # append the body of the message
             msg += '\r\n' + json.dumps(payload)
 
             # DEBUG PRINT
-            # print(msg)
+            # print('>>', msg)
 
-            ws.send(msg)
+            await ws.send(msg)
 
-            # r = await ws.recv()
-            # print(r)
+            producer_task = asyncio.ensure_future(self.send_audio_msg(ws, audio_file_path))
+            consumer_task = asyncio.ensure_future(self.process_response(ws))
+            await asyncio.wait(
+                [producer_task, consumer_task],
+                return_when=asyncio.ALL_COMPLETED,
+            )
 
-            with open(audio_file_path, 'rb') as f_audio:
-                num_chunks = 0
-                while True:
-                    audio_chunk = f_audio.read(self.chunk_size)
-                    if not audio_chunk:
-                        break
-                    num_chunks += 1
 
-                    msg = b'Path: audio\r\n'
-                    msg += b'Content-Type: audio/x-wav\r\n'
-                    msg += b'X-RequestId: ' + bytearray(self.request_id, 'ascii') + b'\r\n'
-                    msg += b'X-Timestamp: ' + bytearray(str(datetime.datetime.now())[:-3], 'ascii') + b'Z\r\n'
-                    # prepend the length of the header in 2-byte big-endian format
-                    msg = len(msg).to_bytes(2, byteorder='big') + msg
+    async def send_audio_msg(self, ws, audio_file_path):
+        with open(audio_file_path, 'rb') as f_audio:
+            num_chunks = 0
+            while True:
+                audio_chunk = f_audio.read(self.chunk_size)
+                if not audio_chunk:
+                    break
+                num_chunks += 1
 
-                    msg += b'\r\n' + audio_chunk
+                # assemble the header for the binary audio message
+                msg = b'Path: audio\r\n'
+                msg += b'Content-Type: audio/x-wav\r\n'
+                msg += b'X-RequestId: ' + bytearray(self.request_id, 'ascii') + b'\r\n'
+                msg += b'X-Timestamp: ' + bytearray(str(datetime.datetime.now())[:-3], 'ascii') + b'Z\r\n'
+                # prepend the length of the header in 2-byte big-endian format
+                msg = len(msg).to_bytes(2, byteorder='big') + msg
+                # append the body of the message
+                msg += b'\r\n' + audio_chunk
 
-                    # DEBUG PRINT
-                    print(msg)
+                # DEBUG PRINT
+                print('>>', msg)
+                sys.stdout.flush()
 
-                    # await ws.send(audio_chunk)
+                try:
                     await ws.send(msg)
-
-                    try:
-                        self.process_response(ws, await ws.recv())
-                    except websockets.exceptions.ConnectionClosed as e:
-                        print('Connection closed: {0}'.format(e))
-
-            # ws.close()
+                except websockets.exceptions.ConnectionClosed as e:
+                    print('Connection closed: {0}'.format(e))
+                    return
 
 
-    async def process_response(self, ws, r):
-        response_path = self.__parse_header_value(r, 'Path')
-
-        if response_path is None:
-            ws.close()
-            return
-
-        if response_path == 'turn.start':
-            self.is_ongoing_turn = True
-        elif response_path == 'speech.startDetected':
-            pass
-        elif response_path == 'speech.hypothesis':
-            self.cur_hypothesis = self.__parse_body_json(r)['Text']
-            print('Current hypothesis: ' + self.cur_hypothesis)
-            pass
-        elif response_path == 'speech.phrase':
-            self.phrase = self.__parse_body_json(r)['Text']
-            print('Predicted phrase: ' + self.cur_hypothesis)
-        elif response_path == 'speech.endDetected':
-            pass
-        elif response_path == 'turn.end':
-            self.is_ongoing_turn = False
-
-        # expect more incoming messages
-        if self.is_ongoing_turn:
+    async def process_response(self, ws):
+        while True:
             try:
-                self.process_response(ws, await ws.recv())
+                response = await ws.recv()
+                print('<<', str(datetime.datetime.now())[:-3] + 'Z\r\n' + response)
+                sys.stdout.flush()
             except websockets.exceptions.ConnectionClosed as e:
                 print('Connection closed: {0}'.format(e))
+                return
+
+            # identify the type of response
+            response_path = self.__parse_header_value(response, 'Path')
+            if response_path is None:
+                print('Error: invalid response header.')
+                ws.close()
+                return
+
+            if response_path == 'turn.start':
+                self.is_ongoing_turn = True
+            elif response_path == 'speech.startDetected':
+                pass
+            elif response_path == 'speech.hypothesis':
+                response_dict = self.__parse_body_json(response)
+                if response_dict is None:
+                    print('Error: no body found in the response. Closing connection.')
+                    ws.close()
+                    return
+                if 'Text' not in response_dict:
+                    print('Error: unexpected response header. Closing connection.')
+                    ws.close()
+                    return
+                self.cur_hypothesis = response_dict['Text']
+                print('Current hypothesis: ' + self.cur_hypothesis)
+            elif response_path == 'speech.phrase':
+                response_dict = self.__parse_body_json(response)
+                if response_dict is None:
+                    print('Error: no body found in the response. Closing connection.')
+                    ws.close()
+                    return
+                if 'RecognitionStatus' not in response_dict:
+                    print('Error: unexpected response header. Closing connection.')
+                    ws.close()
+                    return
+                if response_dict['RecognitionStatus'] == 'Success':
+                    if 'DisplayText' not in response_dict:
+                        print('Error: unexpected response header. Closing connection.')
+                        ws.close()
+                        return
+                    self.phrase = response_dict['DisplayText']
+            elif response_path == 'speech.endDetected':
+                pass
+            elif response_path == 'turn.end':
+                self.is_ongoing_turn = False
+                break
+            else:
+                print('Error: unexpected response type (Path header). Closing connection.')
+                ws.close()
+                return
 
 
     # ---- PRIVATE METHODS ----
@@ -168,14 +202,12 @@ class SpeechClient:
 
 
     def __parse_header_value(self, r, header_to_find):
-        for line in r.split('\n'):
+        for line in r.split('\r\n'):
             if len(line) == 0:
                 break
-            header_prompt = header_to_find + ': '
-            if line.startswith(header_prompt):
-                return line.lstrip(header_prompt)
-
-        print('Error: invalid response header.')
+            header_name = header_to_find + ':'
+            if line.startswith(header_name):
+                return line[len(header_name):].strip()
 
         return None
 
@@ -184,7 +216,7 @@ class SpeechClient:
         body_str = ''
         body_dict = None
 
-        r_as_lines = r.split('\n')
+        r_as_lines = r.split('\r\n')
         for i, line in enumerate(r_as_lines):
             if len(line) == 0:
                 if i < len(r_as_lines) - 1:
@@ -192,9 +224,7 @@ class SpeechClient:
                     try:
                         body_dict = json.loads('\n'.join(r_as_lines[(i + 1):]))
                     except json.JSONDecodeError as e:
-                        print('Error: the response body is not a valid JSON document.')
-                else:
-                    print('Error: no body found in the response.')
+                        print('JSON Decode Error: {0}.'.format(e))
                 break
 
         return body_dict
@@ -205,8 +235,19 @@ if __name__ == '__main__':
         print('Please, provide your key to access the Bing Speech API.')
         exit()
 
+    language = 'en-US'
+    response_format = 'simple'
+    # response_format = 'detailed'
+    audio_file_path = 'data/whatstheweatherlike.wav'
+    # audio_file_path = 'data/test.wav'
+
     client = SpeechClient(sys.argv[1])
 
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(client.send_stt_request('data/whatstheweatherlike.wav'))
+    loop.run_until_complete(client.send_stt_request(language, response_format, audio_file_path))
     loop.close()
+
+    if client.phrase != '':
+        print('\nRecognized phrase: ' + client.phrase)
+    else:
+        print('\nSorry, we were unable to recognize the utterance.')
